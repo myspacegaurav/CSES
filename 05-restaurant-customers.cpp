08774c3d816d1b5d6b6095cb3197483d14282f36
myspacegaurav <g.ankit@zohomail.in>
2026-06-04 19:52:11 +0530

    fixed multiple leaving state

--- 05-restaurant-customers.cpp
+++ 05-restaurant-customers.cpp
@@ -3,37 +3,38 @@
 //if a customer having arrival time a1 and a1 < d0, then it means 0,1 are in the restaurant.
 //so sorting the customers by their departure time, and comparing the arrival time of the next customer with the departure time of the current customer, we can find the maximum number of customers at any given time.
 #include<iostream>
-#include<algorithm>
 #include<vector>
+#include<algorithm>
 
 using namespace std;
 
 int main() {
-  int n;
-  cin >> n;
-  vector<pair<int, int>>customers(n);
-  for(int i = 0; i < n; ++i) {
-    int a, b;
-    cin >> a >> b;
-    customers[i] = {a, b};
-  }
+    int n;
+    cin >> n;
+    vector<int>arrivals(n), departures(n);
 
-  sort(customers.begin(), customers.end(), [](const pair<int, int>&a, const pair<int, int>&b) {
-    return a.second < b.second;
-  });
-
-  int max_customers = 1, current_customers = 1;
-  int mintime = 0; 
-  for(int i = 1; i < n; ++i) {
-    if(customers[i].first < customers[mintime].second) {
-      current_customers++;
-      max_customers = max(max_customers, current_customers);
-    } else {
-      mintime++;
-      current_customers--;
+    for(int i = 0; i < n; ++i) {
+        cin >> arrivals[i] >> departures[i];
     }
-  }
 
-  cout << max_customers << endl;
-  return 0;
+    sort(arrivals.begin(), arrivals.end());
+    sort(departures.begin(), departures.end());
+
+    int i = 0, j = 0;
+    int current_customers = 0, max_customers = 0;
+
+    while(i < n && j < n) {
+        if(arrivals[i] < departures[j]) {
+            current_customers++;
+            max_customers = max(max_customers, current_customers);
+            i++;
+        }
+        else{
+            current_customers--;
+            j++;
+        }
+    }
+
+    cout << max_customers << endl;
+    return 0;
 }
